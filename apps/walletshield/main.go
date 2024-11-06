b555093f6986d92f0b9021c571cf35d1ed96d185
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2024-11-05 21:29:21 -0800

    feat: update apps/walletshield (v0.4.1)

--- apps/walletshield/main.go
+++ apps/walletshield/main.go
@@ -8,31 +8,77 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/fxamacker/cbor/v2"
 
-	"github.com/katzenpost/katzenpost/client"
-	"github.com/katzenpost/katzenpost/client/config"
-	"github.com/katzenpost/katzenpost/client/utils"
+	"github.com/katzenpost/hpqc/hash"
+	"github.com/katzenpost/hpqc/rand"
+
+	"github.com/katzenpost/katzenpost/client2"
+	"github.com/katzenpost/katzenpost/client2/config"
+	"github.com/katzenpost/katzenpost/client2/thin"
+	sConstants "github.com/katzenpost/katzenpost/core/sphinx/constants"
 
 	"github.com/0KnowledgeNetwork/opt/server_plugins/cbor_plugins/http_proxy"
 )
 
+var (
+	timeout          = time.Second * 45
+	ProxyHTTPService = "http_proxy"
+)
+
+func sendRequest(thin *thin.ThinClient, payload []byte) ([]byte, error) {
+	surbID := &[sConstants.SURBIDLength]byte{}
+	_, err := rand.Reader.Read(surbID[:])
+	if err != nil {
+		panic(err)
+	}
+
+	// Select a target service node and compute the DestinationIdHash
+	target, err := thin.GetService(ProxyHTTPService)
+	if err != nil {
+		panic(err)
+	}
+	nodeId := hash.Sum256(target.MixDescriptor.IdentityKey)
+
+	timeoutCtx, _ := context.WithTimeout(context.TODO(), timeout)
+	return thin.BlockingSendMessage(timeoutCtx, payload, &nodeId, target.RecipientQueueID)
+}
+
+type Server struct {
+	log    *log.Logger
+	daemon *client2.Daemon
+	thin   *thin.ThinClient
+}
+
 func main() {
 	var logLevel string
 	var listenAddr string
+	var listenAddrClient string
 	var configPath string
+	var delayStart int
+	var testProbe bool
+	var testProbeCount int
+	var testProbeResponseDelay int
 
 	flag.StringVar(&configPath, "config", "", "file path of the client configuration TOML file")
+	flag.IntVar(&delayStart, "delay_start", 0, "max random seconds to delay start")
 	flag.StringVar(&logLevel, "log_level", "DEBUG", "logging level could be set to: DEBUG, INFO, WARNING, ERROR, CRITICAL")
 	flag.StringVar(&listenAddr, "listen", "", "local socket to listen HTTP on")
+	flag.StringVar(&listenAddrClient, "listen_client", "", "local network address for the client daemon")
+	flag.BoolVar(&testProbe, "probe", false, "send test probes instead of handling requests")
+	flag.IntVar(&testProbeCount, "probe_count", 1, "number of test probes to send")
+	flag.IntVar(&testProbeResponseDelay, "probe_response_delay", 0, "test probe response deplay")
 	flag.Parse()
 
-	if listenAddr == "" {
+	if listenAddr == "" && !testProbe {
 		panic("listen flag must be set")
 	}
 	if configPath == "" {
@@ -45,53 +91,68 @@ func main() {
 		panic(err)
 	}
 	mylog := log.NewWithOptions(os.Stdout, log.Options{
-		Prefix: "daemon",
+		Prefix: "walletshield:",
 		Level:  level,
 	})
 
-	// mixnet client
+	if delayStart > 0 {
+		d := rand.NewMath().Intn(delayStart)
+		mylog.Infof("Delaying start for %d seconds...", d)
+		time.Sleep(time.Duration(d) * time.Second)
+	}
+
+	// start client2 daemon
 	cfg, err := config.LoadFile(configPath)
 	if err != nil {
 		panic(err)
 	}
-	c, err := client.New(cfg)
+
+	if listenAddrClient != "" {
+		cfg.ListenAddress = listenAddrClient
+	}
+
+	d, err := client2.NewDaemon(cfg)
 	if err != nil {
 		panic(err)
 	}
-	session, err := c.NewTOFUSession(context.Background())
+	err = d.Start()
 	if err != nil {
 		panic(err)
 	}
-	ProxyHTTPService := "http_proxy"
-	desc, err := session.GetService(ProxyHTTPService)
+
+	time.Sleep(time.Second * 3) // XXX ugly hack but works: FIXME
+
+	thin := thin.NewThinClient(cfg)
+	err = thin.Dial()
 	if err != nil {
 		panic(err)
 	}
 
 	// http server
 	server := &Server{
-		log:     mylog,
-		session: session,
-		target:  desc,
+		log:    mylog,
+		thin:   thin,
+		daemon: d,
 	}
-	http.HandleFunc("/", server.Handler)
-	http.ListenAndServe(listenAddr, nil)
-}
 
-type Server struct {
-	log     *log.Logger
-	session *client.Session
-	target  *utils.ServiceDescriptor
+	if testProbe {
+		server.SendTestProbes(10*time.Second, testProbeCount, testProbeResponseDelay)
+	} else {
+		http.HandleFunc("/", server.Handler)
+		err := http.ListenAndServe(listenAddr, nil)
+		if err != nil {
+			// Check if the error is related to the port being in use
+			if strings.Contains(err.Error(), "bind: address already in use") {
+				mylog.Errorf("Cannot start server: Listen port %s is already in use. Please check if another instance of walletshield is running or use another port.", listenAddr)
+			} else {
+				mylog.Errorf("Failed to start HTTP server: %s", err)
+			}
+		}
+	}
 }
 
 func (s *Server) Handler(w http.ResponseWriter, req *http.Request) {
-	s.log.Info("received http request")
-
-	// we care about some headers, but not most
-	req.Header = http.Header{
-		"Content-Type":   []string{req.Header.Get("Content-Type")},
-		"Content-Length": []string{req.Header.Get("Content-Length")},
-	}
+	s.log.Infof("Received HTTP request for %s", req.URL)
 
 	myurl, err := url.Parse(req.RequestURI)
 	if err != nil {
@@ -107,16 +168,18 @@ func (s *Server) Handler(w http.ResponseWriter, req *http.Request) {
 	request := new(http_proxy.Request)
 	request.Payload = buf.Bytes()
 
-	s.log.Infof("RAW HTTP REQUEST: %s", string(buf.Bytes()))
+	s.log.Debugf("RAW HTTP REQUEST:\n%s", string(buf.Bytes()))
 
 	blob, err := cbor.Marshal(request)
 	if err != nil {
 		panic(err)
 	}
 
-	rawReply, err := s.session.BlockingSendReliableMessage(s.target.Name, s.target.Provider, blob)
+	rawReply, err := sendRequest(s.thin, blob)
 	if err != nil {
-		fmt.Fprint(w, "custom 404")
+		s.log.Errorf("Failed to send message: %s", err)
+		http.Error(w, "custom 404", http.StatusNotFound)
+		return
 	}
 
 	// use the streaming decoder and simply return the first cbor object
@@ -125,13 +188,71 @@ func (s *Server) Handler(w http.ResponseWriter, req *http.Request) {
 	dec := cbor.NewDecoder(bytes.NewReader(rawReply))
 	err = dec.Decode(response)
 	if err != nil {
+		s.log.Errorf("Failed to decode response: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	if response.Error != "" {
+		s.log.Errorf("Response Error: %s", response.Error)
+	} else {
+		s.log.Infof("Response: %s", response.Payload)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(response.Payload)))
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, string(response.Payload))
+}
+
+func (s *Server) SendTestProbes(d time.Duration, testProbeCount int, testProbeResponseDelay int) {
+	url := fmt.Sprintf("http://nowhere/_/probe/%d", testProbeResponseDelay)
+	req, err := http.NewRequest("GET", url, nil)
+	buf := new(bytes.Buffer)
+	req.Write(buf)
+	request := new(http_proxy.Request)
+	request.Payload = buf.Bytes()
+	blob, err := cbor.Marshal(request)
+	if err != nil {
 		panic(err)
 	}
 
-	s.log.Infof("REPLY: '%s'", rawReply)
+	var packetsTransmitted, packetsReceived int
+	var rttMin, rttMax, rttTotal float64
+	rttMin = math.MaxFloat64
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(rawReply)))
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, string(response.Payload))
+	for {
+		packetsTransmitted++
+		t := time.Now()
+
+		_, err = sendRequest(s.thin, blob)
+		elapsed := time.Since(t).Seconds()
+		if err != nil {
+			s.log.Errorf("Probe failed after %.2fs: %s", elapsed, err)
+		} else {
+			packetsReceived++
+			rttTotal += elapsed
+			if elapsed < rttMin {
+				rttMin = elapsed
+			}
+			if elapsed > rttMax {
+				rttMax = elapsed
+			}
+		}
+
+		packetLoss := float64(packetsTransmitted-packetsReceived) / float64(packetsTransmitted) * 100
+		rttAvg := rttTotal / float64(packetsReceived)
+		if packetsReceived == 0 {
+			rttMin = math.NaN()
+		}
+		s.log.Infof("Probe packet transmitted/received/loss = %d/%d/%.1f%% | rtt min/avg/max = %.2f/%.2f/%.2f s",
+			packetsTransmitted, packetsReceived, packetLoss, rttMin, rttAvg, rttMax)
+
+		// probe indefinitely if testProbeCount is 0
+		if testProbeCount != 0 && packetsTransmitted >= testProbeCount {
+			os.Exit(0)
+		}
+
+		time.Sleep(d)
+	}
 }
